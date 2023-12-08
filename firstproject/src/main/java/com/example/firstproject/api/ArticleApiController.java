243b552f6614abfad961e3adbc83b66dc6f81fce
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2023-12-08 16:29:28 +0900

    서비스 객체로 update() 리팩토링

--- firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -35,26 +35,14 @@ public class ArticleApiController {
                 ResponseEntity.status(HttpStatus.CREATED).body(created) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
-//    // PATCH
-//    @PatchMapping("/api/articles/{id}")
-//    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){
-//        // 1. 수정용 dto 작성
-//        Article article = dto.toEntity();
-//        log.info("id: {}, article: {}", id, article.toString());
-//        // 2. DB에 대상 엔티티 있는지 타깃 조회
-//        Article target = articleRepository.findById(id).orElse(null);
-//        // 3. 잘못된 요청 처리하기(대상 엔티티가 없거나 수정하려는 id가 잘못된 경우)
-//        if(target == null || id != article.getId()) {
-//            // 400, 잘못된 요청 응답!
-//            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-//            // ResponeEntity에 담아서 반환해야만 반환 데이터에 상태 코드를 실어 보낼 수 있음
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);    // 200=OK, 201=CREATED, 400=BAD_REQUEST
-//        }
-//        // 4. 대상 엔티티가 있으면 업데이트 및 정상 응답(200)하기
-//        target.patch(article);  // 기존 데이터에 새 데이터 붙이기
-//        Article updated = articleRepository.save(target);
-//        return ResponseEntity.status(HttpStatus.OK).body(updated);  // 수정 내용 DB에 최종 저장
-//    }
+    // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){
+        Article updated =  articleService.update(id,dto);   // 서비스를 통해 게시글 수정
+        return (updated != null) ?  // 수정되면 정상, 안 되면 오류 응답
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 //    // DELETE
 //    @DeleteMapping("/api/articles/{id}")
 //    public ResponseEntity<Article> delete(@PathVariable Long id){
--- firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
+++ firstproject/src/main/java/com/example/firstproject/service/ArticleService.java
@@ -3,11 +3,12 @@ package com.example.firstproject.service;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service    // 서비스 객체 선언
 public class ArticleService {
     @Autowired
@@ -28,5 +29,23 @@ public class ArticleService {
             return null;
 
         return articleRepository.save(article); // article을 DB에 저장
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        // 1. 수정용 dto 작성
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+        // 2. DB에 대상 엔티티 있는지 타깃 조회
+        Article target = articleRepository.findById(id).orElse(null);
+        // 3. 잘못된 요청 처리하기(대상 엔티티가 없거나 수정하려는 id가 잘못된 경우)
+        if(target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답!
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            // ResponeEntity에 상태와 본문 실어 보내는(응답하는) 역할은 컨트롤러
+            return null;
+        }
+        // 4. 대상 엔티티가 있으면 업데이트 및 정상 응답(200)하기
+        target.patch(article);  // 기존 데이터에 새 데이터 붙이기
+        return articleRepository.save(target);
     }
 }
